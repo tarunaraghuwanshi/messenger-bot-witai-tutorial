1b574f676c24907b96025525b45a7f462e7cd1b7
tarunaraghuwanshi <taruna@ekundali.com>
2018-04-06 09:23:51 -0700

    updated config

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
 'use strict';
 
-const WIT_TOKEN = process.env.WIT_TOKEN || 'OSJI5AOEQRWN7YK56362HQBXHVMQEHCM'
+const WIT_TOKEN ='OSJI5AOEQRWN7YK56362HQBXHVMQEHCM';
 if (!WIT_TOKEN) {
-  throw new Error('Missing WIT_TOKEN. Go to https://wit.ai/docs/quickstart to get one.')
+  throw new Error('Missing WIT_TOKEN. Go to https://wit.ai/docs/quickstart to get one.');
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAACaTM4n1JsBAPKF9tZBr0FPgPk59uKXaPnfJZC0gpGFdnnYGx6SZCip1vmoyMfMHmWwoatY3jIUYhgRp37ZBZAyngw3I9DL5jP4pMYiVnjcGXD7hPkuzllZCPwwtM61v7ZA09BBzh11As8Wq2fbinvrz7HN5BtgSuGJkrSfSZBKu4k72BvV9rhG';
+var FB_PAGE_TOKEN ='EAACaTM4n1JsBAPKF9tZBr0FPgPk59uKXaPnfJZC0gpGFdnnYGx6SZCip1vmoyMfMHmWwoatY3jIUYhgRp37ZBZAyngw3I9DL5jP4pMYiVnjcGXD7hPkuzllZCPwwtM61v7ZA09BBzh11As8Wq2fbinvrz7HN5BtgSuGJkrSfSZBKu4k72BvV9rhG';
+
 if (!FB_PAGE_TOKEN) {
-	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
+	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.');
 }
 
-var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'just_do_it'
+var FB_VERIFY_TOKEN = 'my_voice_is_my_password_verify_me';
 
 module.exports = {
   WIT_TOKEN: WIT_TOKEN,
